26084603570a1ecee12bcf57bdc4a752054c6e01
wahib-elbessali <elbessaliwahib@gmail.com>
2025-02-23 14:54:02 +0000

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -59,6 +59,8 @@ router.post("/logout", (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
+    sameSite: "none",
+    secure: process.env.NODE_ENV === "production",
   });
 
   res.json({ message: "Logout successful" });
