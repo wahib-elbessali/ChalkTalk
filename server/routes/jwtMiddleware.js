9602df0e946e50ff965a23ce6c77b02879d92ec0
wahib-elbessali <elbessaliwahib@gmail.com>
2025-03-27 02:14:49 +0000

    Update jwtMiddleware.js

--- server/routes/jwtMiddleware.js
+++ server/routes/jwtMiddleware.js
@@ -2,10 +2,10 @@ const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
     const token = req.cookies.token;
-    if (!token) return res.status(401).json({ message: 'No token provided' }); //401 unauthorized (semantically it means "unauthenticated")
+    if (!token) return res.status(401).json({ message: 'No token provided' }); //401 unauthorized (wkha ra hiya it means "unauthenticated")
   
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-      if (err) return res.status(401).json({ message: 'Invalid token' }); //401 unauthorized (semantically it means "unauthenticated")
+      if (err) return res.status(401).json({ message: 'Invalid token' });
       req.user = decoded;
       next();
     });
